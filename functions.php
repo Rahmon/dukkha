47b9bb1a774358cb798adfd7e169e3d107edac59
Rahmon <Rahmon@users.noreply.github.com>
2017-12-22 09:20:07 -0200

    Enqueue PaperCSS style

--- functions.php
+++ functions.php
@@ -117,6 +117,8 @@ add_action( 'widgets_init', 'dukkha_widgets_init' );
  * Enqueue scripts and styles.
  */
 function dukkha_scripts() {
+	wp_enqueue_style( 'paper', get_template_directory_uri() . '/css/paper.min.css', array(), '0.3.0' );
+
 	wp_enqueue_style( 'dukkha-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'dukkha-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
