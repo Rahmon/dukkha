f945f1c94050cf9e550a89337f9b9ef2793a909d
Ramon <Rahmon@users.noreply.github.com>
2018-01-12 17:14:56 -0200

    Update footer to show only blog name

--- footer.php
+++ footer.php
@@ -15,15 +15,7 @@
 
 		<footer id="colophon" class="site-footer">
 			<div class="site-info">
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dukkha' ) ); ?>"><?php
-					/* translators: %s: CMS name, i.e. WordPress. */
-					printf( esc_html__( 'Proudly powered by %s', 'dukkha' ), 'WordPress' );
-				?></a>
-				<span class="sep"> | </span>
-				<?php
-					/* translators: 1: Theme name, 2: Theme author. */
-					printf( esc_html__( 'Theme: %1$s by %2$s.', 'dukkha' ), 'dukkha', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+				<p><?php bloginfo( 'name' ); ?></p>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- .paper -->
