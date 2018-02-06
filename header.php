bcdb5e8c568cc42a25c0fb48458ed015772e461f
Rahmon <Rahmon@users.noreply.github.com>
2018-02-06 09:40:51 -0200

    Fix coding standards

--- header.php
+++ header.php
@@ -39,20 +39,23 @@
 
 					$description = get_bloginfo( 'description', 'display' );
 					if ( $description || is_customize_preview() ) : ?>
-						<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+						<h3 class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></h3>
 					<?php
-					endif; ?>
+					endif;
+					?>
 				</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation">
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'menu-1',
-							'menu_id'        => 'primary-menu',
-							'depth'          => 1,
-						) );
-					?>
-				</nav><!-- #site-navigation -->
+				<div class="row flex-center">
+					<nav id="site-navigation" class="main-navigation">
+						<?php
+							wp_nav_menu( array(
+								'theme_location' => 'menu-1',
+								'menu_id'        => 'primary-menu',
+								'depth'          => 1,
+							) );
+						?>
+					</nav><!-- #site-navigation -->
+				</div>
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
