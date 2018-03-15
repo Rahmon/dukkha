271fa9966a43466b851e168383612f5114e6b529
Rahmon <Rahmon@users.noreply.github.com>
2018-03-15 10:11:54 -0300

    Fix footer layout

--- footer.php
+++ footer.php
@@ -13,8 +13,8 @@
 
 			</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer">
-				<div class="site-info">
+			<footer id="colophon" class="site-footer container">
+				<div class="site-info col sm-12">
 					<p><?php bloginfo( 'name' ); ?></p>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
